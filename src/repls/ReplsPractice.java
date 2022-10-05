006c503a6acc3094129af05673708f6dc5056a1a
Qandeel86 <qandeel.wasim@gmail.com>
2022-10-05 12:26:34 -0400

    class16java

---
+++ src/repls/ReplsPractice.java
@@ -0,0 +1,13 @@
+package repls;
+
+
+
+public class ReplsPractice {
+/*Create a variable that will hold the count of all instances of the Main class
+
+Create 3 Object of the class and print value of the count variable;
+*/
+
+}
+
+
