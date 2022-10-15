4f61d78bc64483648bca4782fd0627ee1e37cc74
Qandeel86 <qandeel.wasim@gmail.com>
2022-10-14 23:15:05 -0400

    class 21 & review 8

---
+++ src/reviewclass8/Country.java
@@ -0,0 +1,12 @@
+package reviewclass8;
+
+public class Country {
+    public String name;
+    protected String capital;
+    int population;
+    private String president;
+
+    Country(String name){
+        this.name=name;
+    }
+}
