7f4e5982d00bd0b481b2f3cc339c386f030f2624
Qandeel86 <qandeel.wasim@gmail.com>
2022-11-19 16:36:07 -0500

    class 30-final class

--- src/repls/Main.java
+++ src/repls/Main.java
@@ -1,44 +1,21 @@
 package repls;
 
-import java.util.HashMap;
 import java.util.Iterator;
-import java.util.Map;
+import java.util.TreeSet;
 
 public class Main {
     public static void main(String[] args) {
-        HashMap<String, String> map = new HashMap<String, String>();
-        map.put("ONE","AAA");
-        map.put("TWO","BBB");
-        map.put("THREE","CCC");
-        map.put("FOUR","DDD");
-        map.put("FIVE","EEE");
-
-
-
-        Iterator<Map.Entry<String, String>> itr = map.entrySet().iterator();
-        while (itr.hasNext()) {
-            Map.Entry<String, String> entry = itr.next();
-            System.out.println(entry.getKey() +
-                    " : " + entry.getValue());
-
+        TreeSet map = new TreeSet();
+        map.add("one");
+        map.add("two");
+        map.add("three");
+        map.add("four");
+        map.add("one");
+        Iterator it = map.iterator();
+        while (it.hasNext() )
+        {
+            System.out.print( it.next() + " " );
         }
-
-
-
-        var iterator1 =map.entrySet().iterator();
-
-        while(iterator1.hasNext()){
-            var item=iterator1.next();
-            var key=item.getKey();
-            var value=item.getValue();
-            if(item.getValue().equals("BBB")||item.getValue().equals("CCC")||item.getValue().equals("EEE")){
-
-                System.out.println(item.getKey()+" : "+item.getValue());
-            }
-
-        }
-
-
 
         }
 
