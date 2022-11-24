81892ce9fa54badf1a70972bb480030114462f54
Qandeel86 <qandeel.wasim@gmail.com>
2022-11-23 21:26:01 -0500

    update

--- src/repls/Main.java
+++ src/repls/Main.java
@@ -1,49 +1,25 @@
 package repls;
 
-import java.util.HashMap;
-
 public class Main {
 
-    static void display(HashMap<String,Integer> map){
-        if(map.isEmpty()){
-            System.out.println("map is empty");
-        }else{
-            var entrySet=map.entrySet();
-            for(var item:entrySet){
-                System.out.println(item.getKey()+" : "+item.getValue());
-            }
-        }
-    }
-
     public static void main(String[] args) {
-        /*
-        **NOW IN MAIN METHOD**
-        Create HashMap of String keys and Integer values, add values as below
 
-                call above method with these arguments.
-                then clear the map.
-                and call above method again */
-        HashMap<String,Integer> map=new HashMap<>();
-        map.put("mango",10);
-        map.put("apple",30);
-        map.put("orange",20);
-        display(map);
+            int a=10, b=0;
+           try {
+               int result;
+               System.out.println(result=a/b);
+           } catch (Exception e) {
+               System.out.println("/ by zero");
+           }
 
-        map.clear();
-
-        display(map);
-
-
-
-
-
+        }
 
 
 
 
         }
 
-        }
+
 
 
 
